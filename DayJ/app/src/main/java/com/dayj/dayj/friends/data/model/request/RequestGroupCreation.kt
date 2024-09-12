69c0cd5b5c52dc97e8ed0d827e98bb9708c2a54f
unknown <esteroh@naver.com>
2024-09-12 17:39:29 +0900

    test commit

--- DayJ/app/src/main/java/com/dayj/dayj/friends/data/model/request/RequestGroupCreation.kt
+++
@@ -1,5 +0,0 @@
-package com.dayj.dayj.friends.data.model.request
-
-data class RequestGroupCreation(
-    val groupName: String
-)
--- DayJ/app/src/main/java/com/dayj/dayj/home/CalendarDate.kt
+++
@@ -1,7 +0,0 @@
-package com.dayj.dayj.home
-
-data class CalendarDate(
-    val day: Int,
-    val month: Int,
-    val year: Int
-)
